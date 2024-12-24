bf986b54f7f1fcfb9323a7429d1cb1a80cad4e64
Aabhas Kumar <aabhaskumar@Aabhass-MacBook-Air.local>
2024-12-24 14:16:51 +0530

    app complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
+import { experimental_ppr } from './app/dashboard/layout';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental:{
+  //   ppr: 'incremental',
+  // },
+ 
 };
 
 export default nextConfig;
